195bc764107c8f800b0a9c35fd83473e05784757
Kim-jiii-hye <jine0208@kakao.com>
2022-03-14 10:29:28 +0900

    add undefined page because build error

---
+++ Component/view/ViewCustomer_undefined.html.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { useContext } from "react";
+import { Context } from "../../context";
+
+export default function ViewCustomer_undefined() {
+  
+    return (
+        <div id="customer_contents" className="etc_contents_menu">
+       </div>
+
+    )
+}
